8cd4c13783ea8fe10944a7a9ef52b8ba873cbecf
agripe049 <agripeheber@gmail.com>
2025-03-06 12:08:46 -0300

    Navigate arrumado

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -8,7 +8,7 @@ function Login(){
 
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
-    const navigate = useNavigate
+    const navigate = useNavigate();
     
     const [
         signInWithEmailAndPassword,
