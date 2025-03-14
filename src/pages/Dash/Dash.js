e03b3b3e843a5782069881e5faadd1756ae07e4d
agripe049 <agripeheber@gmail.com>
2025-03-14 00:14:56 -0300

    Suas alterações em src/App.js

--- src/pages/Dash/Dash.js
+++ src/pages/Dash/Dash.js
@@ -3,11 +3,11 @@ import { useEffect, useState } from 'react';
 
 function Dash() {
 
-    const [orcamentoTotal, setOrcamentoTotal] = useState("");
+    const [orcamentoTotal, setOrcamentoTotal] = useState(0);
     const [totalGasto, setTotalGasto] = useState(0);
     const [item, setItem] = useState("");
-    const [gasto, setGasto] = useState("");
-    const [itens, setItens] = useState([])
+    const [gasto, setGasto] = useState(0);
+    const [itens,setItens] = useState([])
 
     useEffect(() => {
         fetch(`http://localhost:5000/orcamentoTotal`, {
@@ -27,24 +27,24 @@ function Dash() {
     const handleAddItem = () => {
         if (item && gasto) {
             const novoGasto = parseFloat(gasto);
-
+            
             // Verifica se o item já existe
             const itemExists = itens.some(existingItem => existingItem.nome === item);
-
+            
             if (itemExists) {
                 alert("Este item já foi adicionado.");
                 return; // Não adiciona o item se já existir
             }
-
+    
             setTotalGasto(prevTotal => prevTotal + novoGasto);
             setItem("");
             setGasto(0);
-
+    
             const itemData = {
                 nome: item,
                 gasto: novoGasto
             };
-
+    
             fetch(`http://localhost:5000/itens`, {
                 method: "POST",
                 headers: {
@@ -52,44 +52,47 @@ function Dash() {
                 },
                 body: JSON.stringify(itemData),
             })
-                .then(response => response.json())
-                .then(data => {
-                    console.log('Item adicionado com sucesso:', data);
-                    // Adicione o novo item à lista de itens com o ID retornado
-                    setItens(prevItens => [...prevItens, { ...itemData, id: data.id }]); // Supondo que o servidor retorne o ID
-                })
-                .catch(error => {
-                    console.error('Erro ao adicionar item:', error);
-                });
+            .then(response => response.json())
+            .then(data => {
+                console.log('Item adicionado com sucesso:', data);
+                // Adicione o novo item à lista de itens com o ID retornado
+                setItens(prevItens => [...prevItens, { ...itemData, id: data.id }]); // Supondo que o servidor retorne o ID
+            })
+            .catch(error => {
+                console.error('Erro ao adicionar item:', error);
+            });
         }
     };
 
     const remove = (index) => {
         const itemToRemove = itens[index]
 
-        {/*Remover o item do estado local*/ }
+        {/*Remover o item do estado local*/}
         setItens(prevItens => prevItens.filter((_, i) => i !== index));
 
-        {/*Subtrair o valor do item removido do totalGasto*/ }
+        {/*Subtrair o valor do item removido do totalGasto*/}
         setTotalGasto(prevTotal => prevTotal - itemToRemove.gasto);
 
         fetch(`http://localhost:5000/itens/${itemToRemove.id}`, {
             method: "DELETE",
         })
-            .then(response => {
-                console.log('Resposta do servidor:', response);
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                console.log('Item removido com sucesso:', data);
-            })
-            .catch(error => {
-                console.error('Erro ao remover item:', error);
-            });
+        .then(response => {
+            console.log('Resposta do servidor:', response);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Item removido com sucesso:', data);
+        })
+        .catch(error => {
+            console.error('Erro ao remover item:', error);
+        });
     }
+
+    
+
 
     const saldoRestante = orcamentoTotal - totalGasto;
 
@@ -101,61 +104,59 @@ function Dash() {
         }).format(value);
     };
 
+
     return (
         <div className={styles.container}>
             <div className={styles.form}>
-                <h1>Gerenciador de Orçamento para Casamentos</h1>
-                {/*Entrada do orçamento Total*/}
-                <div>
-                    <h3>Orçamento Total (R$):</h3>
-                    <input
-                        type="number"
-                        id="orcamentoTotal"
-                        placeholder="Digite o orçamento total"
-                        value={orcamentoTotal || ""}
-                        onChange={(e) => setOrcamentoTotal(e.target.value)}
-                    />
-                </div>
-                {/*Entrada dos itens e dos gastos*/}
-                <div>
-                    <h3>Item:</h3>
-                    <input
-                        type="text"
-                        id="item"
-                        placeholder="Nome do item"
-                        value={item}
-                        onChange={(e) => setItem(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <h3>Gasto (R$):</h3>
-                    <input
-                        type="number"
-                        id="gasto"
-                        placeholder="Valor do Item"
-                        value={gasto || ""}
-                        onChange={(e) => setGasto(e.target.value)}
-                    />
-                </div>
-                <button onClick={handleAddItem}>Adicionar Item</button>
-                {/*Termômetro visual*/}
-                <div className={styles.itemList}>
-                    <h2>Itens Adicionados</h2>
-                    <ul>
-                        {itens.map((item, index) => (
-                            <li key={index}>
-                                <span>{item.nome}</span>
-                                <span className={styles.price}>{formatCurrency(item.gasto)}</span>
-                                <button onClick={() => remove(index)}>Remover</button>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className={styles.footer}>
-                    <p>Total Gasto: {formatCurrency(totalGasto)}</p>
-                    <p>Saldo Restante: {formatCurrency(saldoRestante)}</p>
-                </div>
+            <h1>Gerenciador de Orçamento para Casamentos</h1>
+            {/*Entrada do orçamento Total*/}
+            <div>
+                <h3>Orçamento Total (R$):</h3>
+                <input
+                    type="number"
+                    id="orcamentoTotal"
+                    placeholder="Digite o orçamento total"
+                    value={orcamentoTotal}
+                    onChange={(e) => setOrcamentoTotal(e.target.value)}
+                />
             </div>
+            {/*Entrada dos itens e dos gastos*/}
+            <div>
+                <h3>Item:</h3>
+                <input
+                    type="text"
+                    id="item"
+                    placeholder="Nome do item"
+                    value={item}
+                    onChange={(e) => setItem(e.target.value)}
+                />
+            </div>
+            <div>
+                <h3>Gasto (R$):</h3>
+                <input
+                    type="number"
+                    id="gasto"
+                    placeholder="Valor do Item"
+                    value={gasto}
+                    onChange={(e) => setGasto(e.target.value)}
+                />
+            </div>
+            <button onClick={handleAddItem}>Adicionar Item</button>
+            {/*Termômetro visual*/}
+            <div>
+                <h2>Itens Adicionados</h2>
+                <ul>
+                    {itens.map((item, index) => (
+                        <li key={index}>
+                            {item.nome}: {formatCurrency(item.gasto)}
+                            <button onClick={() => remove(index)}>Remover</button>
+                        </li>
+                    ))}
+                </ul>
+                <p>Total Gasto: {formatCurrency(totalGasto)}</p>
+                <p>Saldo Restante: {formatCurrency(saldoRestante)}</p>
+            </div>
+        </div>
 
         </div>
     )
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,13 +3,11 @@ import styles from './Login.module.css';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../routes/contexts/AuthContext';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const { login } = useAuth();
     
     const [
         signInWithEmailAndPassword,
@@ -23,7 +21,6 @@ function Login() {
     }
 
     if (user) {
-        login(); // Chame a função de login do contexto
         navigate('/dash');
         return null;
     }
@@ -66,7 +63,6 @@ function Login() {
                         Entrar
                     </button>
                 </form>
-                {error && <p>{error.message}</p>} {/* Exibe mensagem de erro, se houver */}
             </div>
         </div>
     );
