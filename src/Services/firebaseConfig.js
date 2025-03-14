be88a04072d4c3b4e010a015407d1624f5df4407
agripe049 <agripeheber@gmail.com>
2025-03-13 23:04:46 -0300

    Projeto finalizado

--- src/Services/firebaseConfig.js
+++ src/Services/firebaseConfig.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// Configuração do Firebase usando variáveis de ambiente
 const firebaseConfig = {
-  apiKey: "AIzaSyAnwigvN0PX4WUQ5NlNL8AIouF1d_0sGwM",
-  authDomain: "gerenciamento-casamento.firebaseapp.com",
-  projectId: "gerenciamento-casamento",
-  storageBucket: "gerenciamento-casamento.firebasestorage.app",
-  messagingSenderId: "196479050428",
-  appId: "1:196479050428:web:c1e60ddec0a51c1c5666b9"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
